1990f1c7d6cc753fa48c0c2a625bc1dae0c845eb
ha-manel <manelhammouche3@gmail.com>
2022-09-06 18:20:33 +0100

    Update relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :recipe
-  has_many :inventory
+  has_many :inventories
 end
