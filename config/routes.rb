83b17ffa701083dfafef316834a4652827bd2131
Jake Felice <jasonfelice0@gmail.com>
2022-09-07 03:54:40 -0700

    Add routes for recipe and recipe_foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get 'user/index'
   get 'user/show'
-  resources :inventories, only: [:index, :show, :new, :create, :destroy]
   devise_for :users
   root "recipes#public"
+  resources :inventories, only: [:index, :show, :new, :create, :destroy]
+  resources :users do
+    resources :recipes do
+      resources :recipe_foods, only: [:new, :create, :destroy, :update, :edit]
+    end
+  end
 end
