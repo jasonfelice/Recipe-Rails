222387aab694d0d64346e4cae6ed96620b736724
Jake Felice <jasonfelice0@gmail.com>
2022-09-08 08:56:05 -0700

    Add destroy method in RecipeFoodsController

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -6,8 +6,8 @@ class RecipeFoodsController < ApplicationController
   end
 
   def create
-    @foods = Food.all
-    @recipe_food = RecipeFood.new(quantity: params[:quantity], food_id: params[:food_id])
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = @recipe.recipe_foods.create(quantity: params[:quantity], food_id: params[:food_id])
     if @recipe_food.save
       redirect_to user_recipe_url(@recipe.user, @recipe), notice: 'Food has been added successfully!'
     else
@@ -22,11 +22,18 @@ class RecipeFoodsController < ApplicationController
   def update
     @recipe_food = RecipeFood.find(params[:id])
     if @recipe_food.update(recipe_update_params)
-      redirect_to user_recipe_url(@recipe_food.recipe.user, @recipe_food.recipe),
-                  notice: 'Recipe food updated Successfully!'
+      redirect_to user_recipe_url(@recipe_food.recipe.user, @recipe_food.recipe), notice: 'Recipe food updated Successfully!'
     else
       redirect_to user_recipe_url(@recipe_food.recipe.user, @recipe_food.recipe), alert: 'Something went wrong!'
     end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = @recipe.recipe_foods.find(params[:id])
+    @recipe_food.destroy
+    flash[:notice] = "Ingredient deleted!"
+    redirect_to user_recipe_url(@recipe.user, @recipe)
   end
 
   private
