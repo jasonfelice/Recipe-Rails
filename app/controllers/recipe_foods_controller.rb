8934a597222b35d93183de4fd09229970cc5e642
Jake Felice <jasonfelice0@gmail.com>
2022-09-08 09:36:31 -0700

    Fix create method in RecipeFoodsController

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -9,7 +9,7 @@ class RecipeFoodsController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = @recipe.recipe_foods.create(quantity: params[:quantity], food_id: params[:food_id])
     if @recipe_food.save
-      redirect_to user_recipe_url(@recipe.user, @recipe), notice: 'Food has been added successfully!'
+      redirect_to user_recipe_url(@recipe.user, @recipe), notice: 'Ingredient has been added successfully!'
     else
       render :new, alert: 'Something went wrong!'
     end
