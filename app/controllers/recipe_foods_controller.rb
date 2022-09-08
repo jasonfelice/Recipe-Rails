a96da61dc7565561f54c09e2a1adcf39114c2e55
Jake Felice <jasonfelice0@gmail.com>
2022-09-07 22:51:16 -0700

    Add update method in RecipeFoodsController

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -9,4 +9,22 @@ class RecipeFoods < ApplicationController
     else
       render :new, alert: "Something went wrong!"
   end
+
+  def edit
+    @recipe_food = RecipeFood.find(params[:id])
+  end
+
+  def update
+    @recipe_food = RecipeFood.find(params[:id])
+    if @recipe_food.update(recipe_update_params)
+      redirect_to user_recipe_url(@recipe_food.recipe.user, @recipe_food.recipe), notice: "Recipe food updated Successfully!"
+    else
+      redirect_to user_recipe_url(@recipe_food.recipe.user, @recipe_food.recipe), alert: "Something went wrong!"
+    end
+  end
+
+  private
+  def recipe_update_params
+    params.permit(:quantity, :food_id)
+  end
 end
