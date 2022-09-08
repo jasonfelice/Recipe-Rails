8da3b828703599110a8ee5fe0eb4b45ad26f04fb
Jake Felice <jasonfelice0@gmail.com>
2022-09-07 22:38:15 -0700

    Set up FoodsController create method

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -3,5 +3,10 @@ class RecipeFoods < ApplicationController
 
   def create
     @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = @recipe.recipe_foods.create(quantity: params[:quantity], food_id: params[:food_id])
+    if @recipe_food.save
+      redirect_to user_recipe_url(@recipe.user, @recipe), notice: "Food has been added successfully!"
+    else
+      render :new, alert: "Something went wrong!"
   end
 end
