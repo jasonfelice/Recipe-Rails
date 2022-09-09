4007edee446b130682e03fa2f7bacee42ea55721
ha-manel <manelhammouche3@gmail.com>
2022-09-09 11:16:07 +0100

    Fix linter errors

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -2,33 +2,42 @@ class ShoppingListsController < ApplicationController
   def generate
     recipe_food = RecipeFood.where(recipe_id: shopping_params[:recipe_id])
     inventory_food = InventoryFood.where(inventory_id: shopping_params[:inventory_id])
+    diff = missing_items(recipe_food, inventory_food)
+    total_price = 0
+    diff.each do |item|
+      total_price += item[:price].to_i
+    end
+    recipe = Recipe.find(shopping_params[:recipe_id])
+    inventory = Inventory.find(shopping_params[:inventory_id])
+    @result = { total_price:, missing_items: diff.length, items: diff, recipe_name: recipe.name,
+                inventory_name: inventory.name, recipe_id: shopping_params[:recipe_id],
+                inventory_id: shopping_params[:inventory_id] }
+    p @result
+    redirect_to shopping_list_path(result: @result)
+  end
+
+  def missing_items(recipe_food, inventory_food)
     diff = []
     found = false
     recipe_food.each do |rec_food|
       inventory_food.each do |inv_food|
-        if rec_food.food_id == inv_food.food_id
-          found = true
-          if rec_food.quantity - inv_food.quantity > 0
-            diff << {name: rec_food.food.name, quantity: rec_food.quantity - inv_food.quantity, measurement_unit: rec_food.food.measurement_unit, price: rec_food.food.price * (rec_food.quantity - inv_food.quantity)}
-          end
-        end
+        next unless rec_food.food_id == inv_food.food_id
+
+        found = true
+        next unless (rec_food.quantity - inv_food.quantity).positive?
+
+        diff << { name: rec_food.food.name, quantity: rec_food.quantity - inv_food.quantity,
+                  measurement_unit: rec_food.food.measurement_unit,
+                  price: rec_food.food.price * (rec_food.quantity - inv_food.quantity) }
       end
       unless found
-        diff << { name: rec_food.food.name, quantity: rec_food.quantity, measurement_unit: rec_food.food.measurement_unit, price: rec_food.food.price * rec_food.quantity }
+        diff << { name: rec_food.food.name, quantity: rec_food.quantity,
+                  measurement_unit: rec_food.food.measurement_unit,
+                  price: rec_food.food.price * rec_food.quantity }
       end
       found = false
     end
-
-    total_price = 0
-    diff.each do |item|
-      total_price = total_price + item[:price].to_i
-    end
-
-    recipe = Recipe.find(shopping_params[:recipe_id])
-    inventory = Inventory.find(shopping_params[:inventory_id])
-    @result = {total_price:, missing_items: diff.length, items: diff, recipe_name: recipe.name , inventory_name: inventory.name, recipe_id: shopping_params[:recipe_id], inventory_id: shopping_params[:inventory_id] }
-    p @result
-    redirect_to shopping_list_path(result: @result)
+    diff
   end
 
   def show
