3c4f0ce9b3f28af706d18bc85fcc834d8d64fce1
ha-manel <manelhammouche3@gmail.com>
2022-09-09 08:45:34 +0100

    Update links

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,7 +16,7 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new(recipe_params)
     @recipe.user_id = current_user.id
     if @recipe.save
-      redirect_to root_path, notice: 'Recipe Added successfully!'
+      redirect_to user_recipes_path(current_user.id), notice: 'Recipe Added successfully!'
     else
       render :show
     end
