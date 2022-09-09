4c9b699d148b8f33ef88c33c385bc0493a297520
Jake Felice <jasonfelice0@gmail.com>
2022-09-09 02:02:56 -0700

    Add recipe_food model specs

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -11,22 +11,22 @@ RSpec.describe Recipe, type: :model do
                         description: 'Test', public: false, user_id: @user.id)
     expect(recipe).to be_valid
   end
-  it 'is should not be valid without a name' do
+  it 'should not be valid without a name' do
     recipe = Recipe.new(name: nil, preparation_time: '6 months', cooking_time: '2 hours', description: 'Test',
                         public: false, user_id: @user.id)
     expect(recipe).to_not be_valid
   end
-  it 'is should not be valid without cooking time' do
+  it 'should not be valid without cooking time' do
     recipe = Recipe.new(name: 'Miso Paste', preparation_time: '6 months', cooking_time: nil, description: 'Test',
                         public: false, user_id: @user.id)
     expect(recipe).to_not be_valid
   end
-  it 'is should not be valid without preparation time' do
+  it 'should not be valid without preparation time' do
     recipe = Recipe.new(name: 'Miso Paste', preparation_time: nil, cooking_time: '2 hours', description: 'Test',
                         public: false, user_id: @user.id)
     expect(recipe).to_not be_valid
   end
-  it 'is should not be valid without a description' do
+  it 'should not be valid without a description' do
     recipe = Recipe.new(name: 'Miso Paste', preparation_time: '6 months', cooking_time: '2 hours',
                         description: nil, public: false, user_id: @user.id)
     expect(recipe).to_not be_valid
